01f61ade6f5301d83eeb8cff3821030fb953f2a5
João Victor <joao_victorflamengo10@outlook.com>
2023-07-10 08:35:25 -0300

    ordem alterada do animacaoScroll

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
@@ -9,10 +8,10 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initAnimaisFetch from "./modules/animais-fetch.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
-initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
@@ -22,3 +21,4 @@ initMenuMobile();
 initFuncionamento();
 initAnimaisFetch();
 initFetchBitcoin();
+initAnimacaoScroll();
